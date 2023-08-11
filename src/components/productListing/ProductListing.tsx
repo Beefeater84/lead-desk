a1fcaf2796220666c20f24471af63a07168f684d
Beefeater <myasoedov.blog@gmail.com>
2023-08-11 22:19:12 +0300

    bug fix

--- src/components/productListing/ProductListing.tsx
+++ src/components/productListing/ProductListing.tsx
@@ -93,7 +93,7 @@ export default function ProductListing() {
           </div>
         </div>
       </section>
-      <AddNewProduct onAddHandler={onAddHandler} />;
+      <AddNewProduct onAddHandler={onAddHandler} />
     </>
   );
 }
